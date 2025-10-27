32be4629a43ee05a083bbe26bb2e80c345824a03
ranaahmed <rnaahmd240@gmail.com>
2025-10-27 14:58:42 +0300

    finished serializer

--- smartmargin/models.py
+++ smartmargin/models.py
@@ -8,7 +8,7 @@ class Ingredient(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    profit_percentage =models.DateField(max_digits=5, decimal_places=2)
+    profit_percentage = models.DecimalField(max_digits=5, decimal_places=2)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     ingredients = models.ManyToManyField(Ingredient,through='ProductIngredient')
     created = models.DateTimeField(auto_now_add=True)
---
+++ smartmargin/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Ingredient,Product,ProductIngredient,Note
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Ingredient
+        fields = '__all__'
+class ProductIngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductIngredient
+        fields ='__all__'
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+class NoteSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields ='__all__'
