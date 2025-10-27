e6cab22b2e47bb3bdbc33085c8f41e489fd0392d
ranaahmed <rnaahmd240@gmail.com>
2025-10-27 14:20:13 +0300

    update finished note model

--- smartmargin/models.py
+++ smartmargin/models.py
@@ -16,3 +16,8 @@ class ProductIngredient(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     ingredient =models.ForeignKey(Ingredient,on_delete=models.CASCADE)
     quanitity = models.DecimalField(max_digits=10,decimal_places=2)
+class Note (models.Model):
+    title = models.CharField(max_length=100)
+    content= models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
