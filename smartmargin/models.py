069825f65554986f2c73d495a59d1d8a493bded7
ranaahmed <rnaahmd240@gmail.com>
2025-10-27 14:17:35 +0300

    finished class ProductIngredient(models.Model):

--- smartmargin/models.py
+++ smartmargin/models.py
@@ -12,3 +12,7 @@ class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     ingredients = models.ManyToManyField(Ingredient,through='ProductIngredient')
     created = models.DateTimeField(auto_now_add=True)
+class ProductIngredient(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    ingredient =models.ForeignKey(Ingredient,on_delete=models.CASCADE)
+    quanitity = models.DecimalField(max_digits=10,decimal_places=2)
