790ccf91a6338777dfaed29d8d312557f37fef45
ranaahmed <rnaahmd240@gmail.com>
2025-10-27 15:25:36 +0300

    finished IngredientViewSet

--- smartmargin/views.py
+++ smartmargin/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets,permissions
+from .models import Ingredient,Product,ProductIngredient,Note
+from .serializers import IngredientSerializer ,ProductSerializer,ProductIngredientSerializer,NoteSerializer
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes =[permissions.IsAuthenticated]
+    def get_queryset(self):
+        return Ingredient.objects.filter(user=self.request.user) 
+    def perform_create(self, serializer):
+       serializer.save(user=self.request.user)
+    
+        
